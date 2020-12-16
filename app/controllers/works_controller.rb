6b31cfeb4e5d5dfe26320d8a382a1c8d7698306e
Naruki <ovoxoomo@icloud.com>
2020-12-16 07:03:15 -0800

    renew works controller and view

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,40 +1,54 @@
 class WorksController < ApplicationController
   before_action :set_work, only: [:edit, :update, :destroy, :show]
-    def index
-      @work = Work.all
-    end
+  def index
+    @works = Work.all
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def new
-      @work = Work.new
-    end
+  def new
+    @work = Work.new
+  end
 
-    def create
-      @work = Work.new(work_params)
+  def create
+    @work = Work.new(work_params)
 
-      if @work.save
-        redirect_to works_path
-      else
-        render "new"
-      end
-    end
-
-    def edit
-    end
-
-    def update
-    end
-
-    def destroy
-      @works.destroy
+    if @work.save
       redirect_to works_path
+    else
+      render "new"
     end
+  end
 
-    private
-    def set_work
-      @work = Work.find(params[:id])
+  def edit
+  end
+
+  def update
+    if @work.update(work_params)
+      redirect_to work_path(@work)
+    else
+      render 'edit'
     end
+  end
+
+  def destroy
+    @work.destroy
+    redirect_to works_path
+  end
+
+  private
+  def set_work
+    @work = Work.find(params[:id])
+  end
+
+  def work_params
+    params[:work].permit(
+      :title,
+      :release,
+      :cast,
+      :description,
+    )
+  end
 
 end
