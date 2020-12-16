64a9dc9134b0c38f9cd6fbb0d96d33ad381410f4
Naruki <ovoxoomo@icloud.com>
2020-12-16 04:58:46 -0800

    genarate homecontroller

---
+++ app/helpers/movielog_helper.rb
@@ -0,0 +1,2 @@
+module MovielogHelper
+end
---
+++ test/controllers/movielog_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MovielogControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
