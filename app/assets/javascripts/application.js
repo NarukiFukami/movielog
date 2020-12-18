39907272a0e533baffe43842deefb075f390e779
Naruki <ovoxoomo@icloud.com>
2020-12-18 05:21:27 -0800

    import design template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,8 @@
+//= require jquery/jquery.min
+//= require bootstrap/js/bootstrap.bundle.min
+//= require jquery.easing/jquery.easing.min
+//= require jquery.easing/jquery.easing.min
+//= require aos/aos
+//= require isotope-layout/isotope.pkgd.min
+//= require owl.carousel/owl.carousel.min
 //= require main
