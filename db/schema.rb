87c12021f48c5afffde1b42a65f75d55182b2726
Naruki <ovoxoomo@icloud.com>
2020-12-21 04:29:41 -0800

    add column to works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_112248) do
+ActiveRecord::Schema.define(version: 2020_12_21_122733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_12_17_112248) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "director_id"
+    t.string "image"
   end
 
 end
