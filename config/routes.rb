cdc7b4752889dc0cd21957f5c2b02a5114def642
Naruki <ovoxoomo@icloud.com>
2020-12-22 05:20:55 -0800

    create user models, table, devisefor sign up sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "home/index" => "home#index"
+  devise_for :users
+  root to: "works#index"
   resources :works
   resources :directors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
